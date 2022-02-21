3c9a7ef0920cb9d514cac96b0fd8ce5cb8c5d6cf
sarpt <mike.m.info@gmail.com>
2022-02-21 21:43:39 +0100

    Implement generators walking through archive contents

--- grep.ts
+++ grep.ts
@@ -26,8 +26,8 @@ const grepVariants = [
   }
 ];
 
-export async function grepFiles(
-  files: string[],
+export async function grepFile(
+  filePath: string,
   {
     options,
     regex,
@@ -38,39 +38,34 @@ export async function grepFiles(
     isMimeType: isMimeTypeCb,
   }
 ): Promise<result[]> {
-  const matches: result[] = [];
+  const grep = grepVariants.find(variant => variant.predicate(filePath, isMimeType))?.cmd || regularGrep;
+  const cmd = [
+    grep,
+    ...options,
+    '-n',
+    regex,
+    filePath,
+  ];
 
-  for (const filePath of files) {
-    const grep = grepVariants.find(variant => variant.predicate(filePath, isMimeType))?.cmd || regularGrep;
-    const cmd = [
-      grep,
-      ...options,
-      '-n',
-      regex,
-      filePath,
-    ];
+  const p = Deno.run({
+    cmd,
+    stdout: 'piped',        
+  });
 
-    const p = Deno.run({
-      cmd,
-      stdout: 'piped',        
-    });
+  const output = await p.output();
+  const textOutput = new TextDecoder().decode(output);
+  const lines = textOutput.split('\n').filter(line => line);
+  if (lines.length === 0) return [];
 
-    const output = await p.output();
-    const textOutput = new TextDecoder().decode(output);
-    const lines = textOutput.split('\n').filter(line => line);
-    if (lines.length === 0) continue;
+  const results: result[] = lines.map(line => {
+    const [lineNumber, ...match] = line.split(':');
+    
+    return {
+      path: filePath,
+      line: Number.parseInt(lineNumber),
+      match: match.join(':'),
+    }
+  });
 
-    const results: result[] = lines.map(line => {
-      const [lineNumber, ...match] = line.split(':');
-      
-      return {
-        path: filePath,
-        line: Number.parseInt(lineNumber),
-        match: match.join(':'),
-      }
-    });
-    matches.push(...results);
-  }
-
-  return matches;
+  return results;
 }
