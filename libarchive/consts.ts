68f94425e6f93fb7cce734a92957c8295a49fcbd
sarpt <mike.m.info@gmail.com>
2022-04-16 01:00:40 +0200

    Use libarchive from github, remove local one from repo

--- libarchive/consts.ts
+++
@@ -1,8 +0,0 @@
-import { ArchiveFormat } from "./types.ts";
-
-export const defaultLibarchivePath = "/usr/lib/libarchive.so"; // ldconfig aliases path; TODO: either parse ld.so.cache or use ldconfig -p to find this
-
-// skip MTREE since libarchive can detect regular text files as MTREE archive (https://github.com/libarchive/libarchive/issues/1051)
-export const defaultSkippableFormats = [
-  ArchiveFormat.ARCHIVE_FORMAT_MTREE,
-];
